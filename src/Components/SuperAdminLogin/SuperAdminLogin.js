154d6f9d512fdc3b980058a6d690b1dd27169f83
Thimal Geekiyanage <thimal.g@getapplova.com>
2023-06-03 08:59:28 +0530

    project and login page init

---
+++ src/Components/SuperAdminLogin/SuperAdminLogin.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import "./SuperAdminLogin.css"
+
+function SuperAdminLogin() {
+  return (
+    <div>
+        <div className='side-box' style={{color:'white', fontSize:'50px'}}>Super Admin Portal</div>
+        <div className='main-box'>
+            <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Log in to OmniGen AI</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SuperAdminLogin;
+
+
+
