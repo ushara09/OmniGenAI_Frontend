a07fda534e3420e0bed57eeaa8d5458d294b6c06
Thimal Geekiyanage <thimal.g@getapplova.com>
2023-06-09 00:35:18 +0530

    added login status popup

--- src/pages/CustomerLoginPage/CustomerLoginPage.js
+++ src/pages/CustomerLoginPage/CustomerLoginPage.js
@@ -1,41 +1,100 @@
-import React from 'react';
-import "./CustomerLoginPage.css"
-import loginPageLogo from "../../resources/images/login-page-logo.png"
+import React, { useState } from "react";
+import "./CustomerLoginPage.css";
+import loginPageLogo from "../../resources/images/login-page-logo.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function CustomerLoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function onSubmit() {
+    if (username === "admin" && password == "admin") {
+      notifySuccess();
+    } else {
+      notifyFail();
+    }
+  }
+
+  function notifySuccess() {
+    toast.success("Login Success !", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+
+  function notifyFail() {
+    toast.error("Login Failed !", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+
   return (
     <div>
-      <img className='logoStyle' src={loginPageLogo}/>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <div>
+        <img className="logoStyle" src={loginPageLogo} />
+      </div>
       <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Log in to OmniGen AI</h3>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter email"
-            />
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Log in to OmniGen AI</h3>
+            <div className="form-group mt-3">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => onSubmit()}
+              >
+                Submit
+              </button>
+            </div>
+            <p className="forgot-password text-right mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
           </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="forgot-password text-right mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
-        </div>
-      </form>
+        </form>
       </div>
     </div>
   );
